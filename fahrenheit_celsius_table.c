58427b1d3c0a85e658ee6d620a41b0a6b064ae96
Michael M <m.migsch@gmail.com>
2019-08-02 17:52:16 +0200

    Update fahrenheit_celsius_table.c

--- fahrenheit_celsius_table.c
+++ fahrenheit_celsius_table.c
@@ -5,7 +5,7 @@ int main()
     /* print Fahrenheit-Celsius table
         for fahr = 0, 20, ..., 300 */
 
-    int fahr, celsius;
+    float fahr, celsius;
     int lower, upper, step;
 
     lower = 0;      // lower limit of temperature table
@@ -16,8 +16,8 @@ int main()
 
     while (fahr <= upper )
     {
-        celsius = 5 * (fahr-32) / 9;
-        printf("%d\t%d\n", fahr, celsius);
+        celsius = (5.0/9.0) * (fahr-32.0);
+        printf("%3.0f %6.1f\n", fahr, celsius);
         fahr += step;
     }
 
